7e7ff6e79b56d0fe33f60c9aff7b68be6b3f90a2
jinxul <ahmadgivekesh@gmail.com>
2020-11-14 23:57:23 +0330

    exportSchema set to false

--- app/src/main/java/com/givekesh/raters/data/source/local/MainDatabase.kt
+++ app/src/main/java/com/givekesh/raters/data/source/local/MainDatabase.kt
@@ -5,7 +5,11 @@ import androidx.room.RoomDatabase
 import com.givekesh.raters.data.entities.coins.CoinsCacheEntity
 import com.givekesh.raters.data.entities.currencies.CurrenciesCacheEntity
 
-@Database(entities = [CurrenciesCacheEntity::class, CoinsCacheEntity::class], version = 1)
+@Database(
+    entities = [CurrenciesCacheEntity::class, CoinsCacheEntity::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class MainDatabase : RoomDatabase() {
     abstract fun currenciesDao(): CurrenciesDao
     abstract fun coinsDao(): CoinsDao
