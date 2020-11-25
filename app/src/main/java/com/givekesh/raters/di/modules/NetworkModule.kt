741262b8e9a9eeb43022a620f32a3c8a51853363
jinxul <ahmadgivekesh@gmail.com>
2020-11-25 05:29:34 +0330

    using SingletonComponent
    ApplicationComponent is deprecated.

--- app/src/main/java/com/givekesh/raters/di/modules/NetworkModule.kt
+++ app/src/main/java/com/givekesh/raters/di/modules/NetworkModule.kt
@@ -7,12 +7,12 @@ import android.net.NetworkRequest
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.components.SingletonComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 class NetworkModule {
 
     @Singleton
--- app/src/main/java/com/givekesh/raters/di/modules/RoomModule.kt
+++ app/src/main/java/com/givekesh/raters/di/modules/RoomModule.kt
@@ -9,12 +9,12 @@ import com.givekesh.raters.utils.Constant
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.components.SingletonComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 object RoomModule {
     @Singleton
     @Provides
