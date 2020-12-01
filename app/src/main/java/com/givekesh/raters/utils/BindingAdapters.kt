d824561fee764ddaf6dc9cb874dea497bb6d6c56
jinxul <ahmadgivekesh@gmail.com>
2020-12-01 11:27:39 +0330

    caching expensive resources

--- app/src/main/java/com/givekesh/raters/utils/BindingAdapters.kt
+++ app/src/main/java/com/givekesh/raters/utils/BindingAdapters.kt
@@ -1,5 +1,7 @@
 package com.givekesh.raters.utils
 
+import android.content.Context
+import android.content.res.TypedArray
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
@@ -7,6 +9,34 @@ import androidx.databinding.BindingAdapter
 import com.givekesh.raters.R
 
 object BindingAdapters {
+
+    @JvmStatic
+    private var TEMP_STRING_ARRAY: Array<String>? = null
+
+    @JvmStatic
+    private fun getTempStringArray(context: Context): Array<String> {
+        var tempArray: Array<String>? = TEMP_STRING_ARRAY
+        if (tempArray == null) {
+            tempArray = context.resources.getStringArray(R.array.flag_alpha2)
+            TEMP_STRING_ARRAY = tempArray
+        }
+        return tempArray
+    }
+
+    @JvmStatic
+    private var TEMP_TYPED_ARRAY: TypedArray? = null
+
+    @JvmStatic
+    private fun getTempTypedArray(context: Context): TypedArray {
+        var tempArray: TypedArray? = TEMP_TYPED_ARRAY
+        if (tempArray == null) {
+            tempArray = context.resources.obtainTypedArray(R.array.flag_drawable)
+            TEMP_TYPED_ARRAY = tempArray
+        }
+        return tempArray
+    }
+
+
     @BindingAdapter("indicator")
     @JvmStatic
     fun setTextColor(view: TextView, indicator: String) {
@@ -21,11 +51,9 @@ object BindingAdapters {
     @BindingAdapter("src")
     @JvmStatic
     fun setImageResource(view: ImageView, alpha2: String) {
-        val resource = view.context.resources
-        val keys = resource.getStringArray(R.array.flag_alpha2)
-        val values = resource.obtainTypedArray(R.array.flag_drawable)
+        val keys = getTempStringArray(view.context)
+        val values = getTempTypedArray(view.context)
         val drawableRes = values.getResourceId(keys.indexOf(alpha2), android.R.color.transparent)
         view.setImageResource(drawableRes)
-        values.recycle()
     }
 }
